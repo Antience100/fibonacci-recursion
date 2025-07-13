c6a2e5f88cd8d74159b24cb51ce880fcb7640ef0
Antience100 <190290455+Antience100@users.noreply.github.com>
2025-07-13 15:14:35 -0400

    Used merge sort to complete

---
+++ fibonacciMerge.js
@@ -0,0 +1,29 @@
+let sequenceOne = [3, 2, 1, 13, 8, 5, 0, 1];
+let sequenceTwo = [105, 79, 100, 110];
+
+function merge(left, right) {
+  let sortedArray = [];
+
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      sortedArray.push(left.shift());
+    } else {
+      sortedArray.push(right.shift());
+    }
+  }
+
+  return [...sortedArray, ...left, ...right];
+}
+function mergeSort(array) {
+  if (array.length <= 1) return array;
+
+  let middle = Math.floor(array.length / 2);
+
+  let left = mergeSort(array.slice(0, middle));
+  let right = mergeSort(array.slice(middle));
+
+  return merge(left, right);
+}
+
+console.log(mergeSort(sequenceOne));
+console.log(mergeSort(sequenceTwo));
