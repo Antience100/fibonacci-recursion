8ab56f2b33268330ced7ae54201f31b277d43312
Antience100 <190290455+Antience100@users.noreply.github.com>
2025-07-13 14:46:14 -0400

    Completed recursion project using RECURSION

--- fibonacci.js
+++ fibonacci.js
@@ -1,19 +1,29 @@
-
 function fibs(num) {
+  if (num <= 0) return [];
+  if (num === 1) return [0];
 
-    if (num <= 0) {
-        return [];
-    } else if (num === 1) {
-        return [0];
-    }
+  let sequence = [0, 1];
 
-    let sequence = [0, 1];
+  for (let i = 2; i < num; i++) {
+    sequence.push(sequence[i - 1] + sequence[i - 2]);
+  }
 
-    for (let i = 2; i < num; i++) {
-        sequence.push(sequence[i - 1] + sequence[i - 2]);
-    }
-
-    return sequence;
+  return sequence;
 }
+// console.log(fibs(8));
 
-console.log(fibs(8));
+function fibsRec(num) {
+  if (num <= 0) return [];
+  if (num === 1) return [1];
+  if (num === 2) return [0, 1];
+
+  function getFibSequence(n, sequence = [0, 1]) {
+    if (sequence.length >= n) return sequence;
+    sequence.push(
+      sequence[sequence.length - 1] + sequence[sequence.length - 2]
+    );
+    return getFibSequence(n, sequence);
+  }
+  return getFibSequence(num);
+}
+console.log(fibsRec(8));
